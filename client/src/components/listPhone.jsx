3daddbcf7f880b88ae7340667eb3bf11417c122a
MaxDenisenko <MaxDenisenko2013@yandex.ru>
2023-12-01 19:12:58 +0300

    add search field

--- client/src/components/listPhone.jsx
+++ client/src/components/listPhone.jsx
@@ -2,9 +2,22 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import  {AuthLogoutAction} from '../redux/actions/auth.action'
 import { GetZapis } from "../redux/actions/zapis.action";
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridToolbarQuickFilter } from '@mui/x-data-grid';
 import AddForm from "./addForm";
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
+
+function QuickSearchToolbar() {
+    return (
+      <Box
+        sx={{
+          p: 0.5,
+          pb: 0,
+        }}
+      >
+        <GridToolbarQuickFilter placeholder="Поиск"/>
+      </Box>
+    );
+  }
 
 
 const ListPhone = () => {
@@ -18,12 +31,11 @@ const ListPhone = () => {
     const handleClickOpen = () => {
         setModalOpen(true);
       };
-
     const columns = [
         {field: 'id', headerName: 'ID', width: 20},
-        {field: 'phone', headerName: 'Номер телефона', width: 140},
-        {field: 'plus',type: 'number', headerName: 'Положительных', width: 120},
-        {field: 'minus',type: 'number', headerName: 'Отрицательных', width: 120},
+        {field: 'phone', headerName: 'Телефон', width : 130},
+        {field: 'plus',type: 'number', headerName: '+', width: 75},
+        {field: 'minus',type: 'number', headerName: '-', width: 75},
 ]
 
     return <>
@@ -40,11 +52,12 @@ const ListPhone = () => {
             columns={columns}
             initialState={{
                 pagination: {
-                  paginationModel: { page: 0, pageSize: 39 },
+                  paginationModel: { page: 0, pageSize: 10 },
                 },
               }}
-              pageSizeOptions={[39, 40]}
-              checkboxSelection
+              pageSizeOptions={[10, 20, 30, 40]}
+              autoHeight
+            slots={{ toolbar: QuickSearchToolbar }}
             />}
     </div>
     
