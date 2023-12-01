21c0cb3aceda559e0f5437aa4b0682a1a4a9a44d
MaxDenisenko <MaxDenisenko2013@yandex.ru>
2023-12-01 09:20:59 +0300

    fix logout

--- client/src/redux/reducers/authReducer.js
+++ client/src/redux/reducers/authReducer.js
@@ -10,7 +10,7 @@ const authReducers = (state = initialState, action) => {
         case AUTH_DATA_USER: 
             return {...state, ...action.payload, isLogin: !state.isLogin}
         case AUTH_LOGOUT:
-            return {state, isLogin: false, isLoading: false}
+            return {...state, auth:{}, isLogin: false, isLoading: false, zapis:{}, }
         case LOADING:
             return {...state, isLoading: !state.isLoading}
         default:
