4331a451fa44192fe0908d33af40b13a456a724e
MaxDenisenko <MaxDenisenko2013@yandex.ru>
2023-12-01 09:48:30 +0300

    clean state

--- client/src/redux/reducers/zapisReducer.js
+++ client/src/redux/reducers/zapisReducer.js
@@ -1,9 +1,12 @@
-import { DATA_ZAPIS } from "../const"
+import { DATA_ZAPIS, AUTH_LOGOUT } from "../const"
+const initialState = []
 
-const zapisReducers = (state=[], action) => {
+const zapisReducers = (state=initialState, action) => {
     switch (action.type) {
         case DATA_ZAPIS:
             return {...state, zapis: [...action.payload]}
+        case AUTH_LOGOUT:
+            return initialState
         default:
             return state
     }
