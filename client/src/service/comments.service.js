d239c7c8124da2e413d3174802c5121d5b916ab9
MaxDenisenko <MaxDenisenko2013@yandex.ru>
2023-12-06 21:14:03 +0300

    fix get_post

--- client/src/service/comments.service.js
+++ client/src/service/comments.service.js
@@ -2,7 +2,7 @@ import api from "../api";
 
 class  CommentsService {
     static async getComments(phone){
-        return api.get('/comment',{phone})
+        return api.post('/comments',{phone})
     }
     static async createComments(date, phone, realtorFIO, plusminus, message) {
         return api.post('/comment',{date, phone, realtorFIO, plusminus, message})
--- server/routes/comments.routes.js
+++ server/routes/comments.routes.js
@@ -4,7 +4,7 @@ const router = new Router()
 const { getComments, createComment} = require('../controllers/comments.controller')
 const authMidleware = require('../middleware/auth.midleware')
 
-router.get('/comment',authMidleware, getComments)
+router.post('/comments',authMidleware, getComments)
 router.post('/comment',authMidleware, createComment)
 
 
