24f0c17b93d159975a622e880802c9b4d57cb38f
MaxDenisenko <MaxDenisenko2013@yandex.ru>
2023-12-07 18:28:01 +0300

    fix sequilize

--- server/services/comments.service.js
+++ server/services/comments.service.js
@@ -14,9 +14,9 @@ class CommentsService {
         }
         const createComment = await commentsModule.create({date, phone, realtorFIO, plusminus, message})
         if (plusminus === 'Отрицательный') {
-            await zapisModule.update({minus: minus++},{where: phone})
+            await zapisModule.update({minus: sequelize.literal('minus + 1')},{where: phone})
         }
-        await zapisModule.update({plus: plus++},{where: phone})
+        await zapisModule.update({plus: sequelize.literal('plus + 1')},{where: phone})
             
         return createComment
     }
