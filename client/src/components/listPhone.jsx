22c811f6ed4219e57cb7b6951e1f71fd5650b364
MaxDenisenko <MaxDenisenko2013@yandex.ru>
2023-12-07 17:28:39 +0300

    fix

--- client/src/components/listPhone.jsx
+++ client/src/components/listPhone.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import  {AuthLogoutAction} from '../redux/actions/auth.action'
+import { AuthLogoutAction } from '../redux/actions/auth.action'
 import { GetZapis } from "../redux/actions/zapis.action";
 import AddForm from "./addForm";
 import { Box, Button, Collapse, IconButton, Typography } from "@mui/material";
@@ -10,98 +10,59 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import AddCommentIcon from '@mui/icons-material/AddComment';
 import AddCommentForm from "./addCommentForm";
 import { GetComments } from "../redux/actions/comments.action";
 
-// const message = [
-//   {
-//     id:1,
-//     date:'01.11.2023',
-//     realtorFIO:'Иванов Иван Иванович',
-//     plusminus:'Отрицательный',
-//     message:'Иван совсем не брежно относится к квартире'
-//   },
-//   {
-//     id:2,
-//     date:'01.11.2023',
-//     realtorFIO:'Иванов Иван Иванович',
-//     plusminus:'Отрицательный',
-//     message:'Иван совсем не брежно относится к квартире'
-//   },
-//   {
-//     id:3,
-//     date:'01.11.2023',
-//     realtorFIO:'Иванов Иван Иванович',
-//     plusminus:'Положительный',
-//     message:'Иван совсем не брежно относится к квартире'
-//   },
-//   {
-//     id:4,
-//     date:'01.11.2023',
-//     realtorFIO:'Иванов Иван Иванович',
-//     plusminus:'Положительный',
-//     message:'Иван совсем не брежно относится к квартире'
-//   },
-//   {
-//     id:5,
-//     date:'01.11.2023',
-//     realtorFIO:'Иванов Иван Иванович',
-//     plusminus:'Отрицательный',
-//     message:'Иван совсем не брежно относится к квартире'
-//   },
-// ]
-
 const ListPhone = () => {
-    const dispatch = useDispatch()
-    const zapis = useSelector(state => state.zapis.zapis)
-    useEffect(()=> {
-        dispatch(GetZapis())
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
-    const [modalOpen, setModalOpen] = useState(false);
-    const handleClickOpen = () => {
-        setModalOpen(true);
-      };
+  const dispatch = useDispatch()
+  const zapis = useSelector(state => state.zapis.zapis)
+  useEffect(() => {
+    dispatch(GetZapis())
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  const [modalOpen, setModalOpen] = useState(false);
+  const handleClickOpen = () => {
+    setModalOpen(true);
+  };
 
-    return <>
-    <Box sx={{margin: "10px auto", width:"100%", maxWidth:650}}>
-        <Box sx={{ display: "flex", justifyContent: "space-between", marginBottom:"5px"}}>
-            <Button variant="outlined" onClick={handleClickOpen}>
-                Добавить телефон
-            </Button>
-            <AddForm modalOpen={modalOpen} setModalOpen={setModalOpen}/>
-            <Button variant="contained" onClick={()=>dispatch(AuthLogoutAction())}>Выйти</Button>
-        </Box>
-        <Box sx={{height: "100%"}}>
-          <TableContainer component={Paper}>
-            <Table sx={{maxWidth: "100%", minWidth:350}}>
-              <TableHead>
-                <TableRow>
-                  <TableCell>ID</TableCell>
-                  <TableCell></TableCell>
-                  <TableCell>Телефон</TableCell>
-                  <TableCell>+</TableCell>
-                  <TableCell>-</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {zapis && zapis.map((item) => (
-                  <Row key={item.id} row={item}/>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Box>
+  return <>
+    <Box sx={{ margin: "10px auto", width: "100%", maxWidth: 650 }}>
+      <Box sx={{ display: "flex", justifyContent: "space-between", marginBottom: "5px" }}>
+        <Button variant="outlined" onClick={handleClickOpen}>
+          Добавить телефон
+        </Button>
+        <AddForm modalOpen={modalOpen} setModalOpen={setModalOpen} />
+        <Button variant="contained" onClick={() => dispatch(AuthLogoutAction())}>Выйти</Button>
       </Box>
-    </>
+      <Box sx={{ height: "100%" }}>
+        <TableContainer >
+          <Table sx={{ maxWidth: "100%", minWidth: 350 }}>
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell></TableCell>
+                <TableCell>Телефон</TableCell>
+                <TableCell>+</TableCell>
+                <TableCell>-</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {zapis && zapis.map((item) => (
+                <Row key={item.id} row={item} />
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Box>
+  </>
 }
 
-function Row (props) {
-  const {row} = props
+function Row(props) {
+  const { row } = props
   const [open, setOpen] = useState(false)
   const [openModalAddComment, setOpenModalAddComment] = useState(false)
   const realtorFIO = useSelector(state => state.auth.user.email)
@@ -109,7 +70,7 @@ function Row (props) {
 
 
   const getCommentsAndOpen = (phone) => {
-    if(open) {
+    if (open) {
       setOpen(!open)
     }
     setOpen(!open)
@@ -119,54 +80,54 @@ function Row (props) {
   return (
     <>
       <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-            <TableCell>{row.id}</TableCell>
-            <TableCell style={{padding:0}}>
-              <IconButton
-                size="small"
-                onClick={()=>getCommentsAndOpen(row.phone)}
-                >
-                {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-              </IconButton>
-            </TableCell>
-            <TableCell>{row.phone} <IconButton size="small" onClick={()=>setOpenModalAddComment(true)}>
-              <AddCommentIcon /></IconButton>
-              <AddCommentForm modalCommentsOpen={openModalAddComment} setModalCommentsOpen={setOpenModalAddComment} phone={row.phone} realtorFIO={realtorFIO}/>
-            </TableCell>
-            <TableCell>{row.plus}</TableCell>
-            <TableCell>{row.minus}</TableCell>
-        </TableRow>
-        <TableRow>
+        <TableCell>{row.id}</TableCell>
+        <TableCell style={{ padding: 0 }}>
+          <IconButton
+            size="small"
+            onClick={() => getCommentsAndOpen(row.phone)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell>{row.phone} <IconButton size="small" onClick={() => setOpenModalAddComment(true)}>
+          <AddCommentIcon /></IconButton>
+          <AddCommentForm modalCommentsOpen={openModalAddComment} setModalCommentsOpen={setOpenModalAddComment} phone={row.phone} realtorFIO={realtorFIO} />
+        </TableCell>
+        <TableCell>{row.plus}</TableCell>
+        <TableCell>{row.minus}</TableCell>
+      </TableRow>
+      <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={5}>
           <Collapse in={open} timeout='auto' unmountOnExit>
-            <Box sx={{ margin: 0}}>
+            <Box sx={{ margin: 0 }}>
               <Typography variant="h6" gutterBottom component="div">
                 Отзывы
               </Typography>
-              <Table size="small" sx={{maxWidth:"100%"}}>
+              <Table size="small" sx={{ maxWidth: "100%", border: 1 }} >
                 <TableBody>
-                  {row.message 
-                  ? row.message.map((item) => (<Box sx={{border: 1}}>
-                    <TableRow key={item.id}>
-                      <TableCell>{item.date}</TableCell>
-                      <TableCell>{item.realtorFIO}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell colSpan="2">{item.plusminus}</TableCell>
-                    </TableRow>
-                    <TableRow>
-                      <TableCell colSpan="2">{item.message}</TableCell>
-                    </TableRow>
 
-                    </Box>
-                  ))
-                : <TableCell><Typography variant="h8" gutterBottom component="div">Еще не оставили ни одного комментария</Typography></TableCell>
-                }
+                  {row.message
+                    ? row.message.map((item) => (<>
+                      <TableRow key={item.id} >
+                        <TableCell>{item.date}</TableCell>
+                        <TableCell>{item.realtorFIO}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell colSpan="2">{item.plusminus}</TableCell>
+                      </TableRow>
+                      <TableRow>
+                        <TableCell colSpan="2">{item.message}</TableCell>
+                      </TableRow>
+                    </>
+                    ))
+                    : <TableRow><Typography variant="h8" gutterBottom component="div">Еще не оставили ни одного комментария</Typography></TableRow>
+                  }
                 </TableBody>
               </Table>
             </Box>
           </Collapse>
         </TableCell>
-        </TableRow>
+      </TableRow>
     </>
   )
 }
