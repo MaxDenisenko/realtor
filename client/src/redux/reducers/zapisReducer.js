a9ff27d38f3559d00713dd7b8c4116b71a50b958
MaxDenisenko <MaxDenisenko2013@yandex.ru>
2023-12-11 23:55:32 +0300

    add getComments for search

--- client/src/redux/reducers/zapisReducer.js
+++ client/src/redux/reducers/zapisReducer.js
@@ -1,22 +1,30 @@
 import { DATA_ZAPIS, AUTH_LOGOUT, DATA_COMMENTS, SEARCH_PHONE } from "../const"
 const initialState = []
 
-const zapisReducers = (state=initialState, action) => {
+const zapisReducers = (state = initialState, action) => {
     switch (action.type) {
         case DATA_ZAPIS:
-            return {...state, zapis: [...action.payload]}
+            return { ...state, zapis: [...action.payload] }
         case AUTH_LOGOUT:
             return initialState
         case DATA_COMMENTS:
-            return {...state, zapis: state.zapis.map((zapis) => {
-                if(zapis.phone === action.payload[0].phone) {
-                    return {...zapis, message:[...action.payload]}
-                }
-                return zapis
-            })}
+            return {
+                ...state, zapis: state.zapis.map((zapis) => {
+                    if (zapis.phone === action.payload[0].phone) {
+                        return { ...zapis, message: [...action.payload] }
+                    }
+                    return zapis
+                }), searchPhone: state.searchPhone.map((searchPhone => {
+                    if (searchPhone.phone === action.payload[0].phone) {
+                        return { ...searchPhone, message: [...action.payload] }
+                    }
+                    return searchPhone
+                }))
+            }
         case SEARCH_PHONE:
-            return {...state,
-            searchPhone:[...state.zapis].filter(item => item.phone.includes(action.payload))
+            return {
+                ...state,
+                searchPhone: [...state.zapis].filter(item => item.phone.includes(action.payload))
             }
         default:
             return state
